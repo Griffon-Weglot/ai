1d311c5bbf718b5537606cde639d2d339783ff8b
Antoine Griffon <antoine@weglot.com>
2025-09-12 16:38:19 +0200

    feat(profiler): trace and display AgentInterface calls

--- src/ai-bundle/config/services.php
+++ src/ai-bundle/config/services.php
@@ -11,6 +11,7 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use Symfony\AI\Agent\AgentInterface;
 use Symfony\AI\Agent\StructuredOutput\AgentProcessor as StructureOutputProcessor;
 use Symfony\AI\Agent\StructuredOutput\ResponseFormatFactory;
 use Symfony\AI\Agent\StructuredOutput\ResponseFormatFactoryInterface;
@@ -24,6 +25,7 @@ use Symfony\AI\Agent\Toolbox\ToolResultConverter;
 use Symfony\AI\AiBundle\Command\AgentCallCommand;
 use Symfony\AI\AiBundle\Command\PlatformInvokeCommand;
 use Symfony\AI\AiBundle\Profiler\DataCollector;
+use Symfony\AI\AiBundle\Profiler\TraceableAgent;
 use Symfony\AI\AiBundle\Profiler\TraceableToolbox;
 use Symfony\AI\AiBundle\Security\EventListener\IsGrantedToolAttributeListener;
 use Symfony\AI\Chat\Command\DropStoreCommand as DropMessageStoreCommand;
@@ -173,6 +175,13 @@ return static function (ContainerConfigurator $container): void {
                 service('expression_language')->nullOnInvalid(),
             ])
             ->tag('kernel.event_listener')
+
+        ->set('ai.traceable_agent', TraceableAgent::class)
+        ->decorate(AgentInterface::class, priority: 5)
+        ->args([
+            service('.inner'),
+            service('ai.data_collector'),
+        ])
 
         // profiler
         ->set('ai.data_collector', DataCollector::class)
